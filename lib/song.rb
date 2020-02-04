590adceeb3e68b5060afe3bbe712d7e79d775a14
Matthew Anderson <anderson.matt1397@gmail.com>
2020-02-04 02:53:16 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -40,5 +40,11 @@ class Song
   end
   
   def Song.new_from_filemane
-    
+    song = self.new 
+    song.name = (name.split(" - ")[1].chomp(".mp3"))
+    song.artist_name = (name.split(" - ")[0])
+    song
+  end
+  
+  def Song.create_from_filename
     
